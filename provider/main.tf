03b5162ae7eeac2d74ae5887002e442512c73812
Anastasia <Nastya170204@yandex.ru>
2023-10-17 20:48:30 +0700

    structure (first)

---
+++ provider/main.tf
@@ -0,0 +1,3 @@
+resource "example_server" "my-server" {
+    address = "1.2.3.4"
+} 
---
+++ provider/resource_server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+)
+
+func resourceServer() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceServerCreate,
+		Read:   resourceServerRead,
+		Update: resourceServerUpdate,
+		Delete: resourceServerDelete,
+
+		Schema: map[string]*schema.Schema{
+			"address": &schema.Schema{
+				Type:     schema.TypeString,
+				Required: true,
+			},
+		},
+	}
+}
+
+func resourceServerCreate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceServerRead(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceServerUpdate(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
+
+func resourceServerDelete(d *schema.ResourceData, m interface{}) error {
+	return nil
+}
