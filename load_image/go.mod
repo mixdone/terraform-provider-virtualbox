f1d01e3aa08e62361e23a3111b4b963c7950f375
Michael Diza <m.diza@g.nsu.ru>
2023-11-04 18:21:22 +0700

    load image with error

---
+++ load_image/go.mod
@@ -0,0 +1,14 @@
+module main.go
+
+go 1.21.3
+
+require (
+	github.com/mixdone/virtualbox-go v0.0.0-20190704084130-9d36166b6742
+	github.com/sirupsen/logrus v1.9.3
+)
+
+require (
+	github.com/golang/glog v1.1.2 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	gopkg.in/d4l3k/messagediff.v1 v1.2.1 // indirect
+)
---
+++ load_image/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	log "github.com/sirupsen/logrus"
+
+	vbg "github.com/mixdone/virtualbox-go"
+)
+
+func main() {
+	// create VM with name(vmNmae)
+	vmName, CPUs, memory := "Test", 1, 1024
+	CreateVM(vmName, CPUs, memory)
+}
+
+func CreateVM(vmName string, CPUs, memory int) (string, *vbg.VBox, *vbg.VirtualMachine) {
+	dirName, err := os.MkdirTemp("", "vbm")
+	if err != nil {
+		log.Fatalf("Tempdir creation failed: %s", err.Error())
+	}
+	defer os.RemoveAll(dirName)
+
+	vb := vbg.NewVBox(vbg.Config{
+		BasePath: dirName,
+	})
+
+	// Жесткий диск
+	ctr := vbg.StorageController{
+		Name: "SATA Controller",
+		Type: vbg.SATA,
+	}
+
+	sata := vbg.StorageControllerAttachment{
+		Type:   vbg.SATA,
+		Name:   "SATA Controller",
+		Port:   0,
+		Device: 0,
+	}
+
+	disk1 := vbg.Disk{
+		Path:       filepath.Join(dirName, "disk1.vdi"),
+		Format:     vbg.VDI,
+		SizeMB:     10,
+		Type:       "hdd",
+		Controller: sata,
+	}
+
+	// Загрузчик для образа
+	ctr1 := vbg.StorageController{
+		Name: "IDE Controller",
+		Type: vbg.IDE,
+	}
+
+	ide := vbg.StorageControllerAttachment{
+		Type:   vbg.IDE,
+		Name:   "IDE Controller",
+		Port:   1,
+		Device: 0,
+	}
+
+	disk2 := vbg.Disk{
+		Path:       "ubuntu-23.10.1-desktop-amd64.iso",
+		Type:       "dvddrive",
+		Controller: ide,
+	}
+
+	log.Infof("disk: %s", string(disk1.Type))
+	err = vb.CreateDisk(&disk1)
+	if err != nil {
+		log.Fatalf("Disk creation failed: %s", err.Error())
+	}
+
+	// Параметры вируальной машины
+	spec := &vbg.VirtualMachineSpec{
+		Name:   vmName,
+		OSType: vbg.Ubuntu64,
+		CPU:    vbg.CPU{Count: CPUs},
+		Memory: vbg.Memory{SizeMB: memory},
+		Disks:  []vbg.Disk{disk1, disk2},
+	}
+
+	vm := &vbg.VirtualMachine{
+		Spec: *spec,
+	}
+
+	fmt.Println("Creating VM with CPU and memory", vm.Spec.CPU, vm.Spec.Memory)
+
+	err = vb.CreateVM(vm)
+	if err != nil {
+		log.Fatalf("VM creation failed: %s", err.Error())
+	}
+
+	fmt.Println("Created VM with CPU and memory", vm.Spec.CPU, vm.Spec.Memory)
+
+	err = vb.RegisterVM(vm)
+	if err != nil {
+		log.Fatalf("Failed registering vm")
+	}
+
+	fmt.Println("Register VM with CPU and memory", vm.Spec.CPU, vm.Spec.Memory)
+
+	vb.SetCPUCount(vm, vm.Spec.CPU.Count)
+	vb.SetMemory(vm, vm.Spec.Memory.SizeMB)
+
+	// подключение диска к виртуальной машине
+	err = vb.AddStorageController(vm, ctr)
+	if err != nil {
+		log.Fatalf("ctr: %s", err.Error())
+	}
+
+	err = vb.AttachStorage(vm, &disk1)
+	if err != nil {
+		log.Fatalf("strorage: %s", err.Error())
+	}
+
+	// Подключаем установочный образ диска
+	err = vb.AddStorageController(vm, ctr1)
+	if err != nil {
+		log.Fatalf("ctr1: %s", err.Error())
+	}
+
+	err = vb.AttachStorage(vm, &disk2)
+	if err != nil {
+		log.Fatalf("storage %s", err.Error())
+	}
+
+	return dirName, vb, vm
+}
+
+func GetVMInfo(name string) (machine *vbg.VirtualMachine, err error) {
+	vb := vbg.NewVBox(vbg.Config{})
+	return vb.VMInfo(name)
+}
