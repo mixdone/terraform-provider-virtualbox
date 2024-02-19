30a1cc5c7f6cc20afd32ecc88197aed0b0054aeb
Michael Diza <m.diza@g.nsu.ru>
2024-02-19 20:04:21 +0700

    mergeable firt network adapter version

--- examples/resources/main.tf
+++ examples/resources/main.tf
@@ -1,6 +1,6 @@
 
 resource "virtualbox_server" "VM_without_image" {
-    count     = 1
+    count     = 0
     name      = format("VM_without_image-%02d", count.index + 1)
     basedir = format("VM_without_image-%02d", count.index + 1)
     cpus      = 3
@@ -10,7 +10,7 @@ resource "virtualbox_server" "VM_without_image" {
 }
 
 resource "virtualbox_server" "bad_VM_example" {
-    count     = 1
+    count     = 0
     name      = format("VM_without_image-%02d", count.index + 1)
     basedir = format("VM_without_image-%02d", count.index + 1)
     cpus      = 30
@@ -18,6 +18,7 @@ resource "virtualbox_server" "bad_VM_example" {
     status = "asdfasdf"
     os_id = "Windows7_64"
 }
+
 
 resource "virtualbox_server" "VM_VDI" {
     count     = 0
@@ -28,6 +29,38 @@ resource "virtualbox_server" "VM_VDI" {
     url =  "https://github.com/ccll/terraform-provider-virtualbox-images/releases/download/ubuntu-15.04/ubuntu-15.04.tar.xz"
     status = "poweroff"
     vdi_size = 25000
+}
+
+
+
+resource "virtualbox_server" "VM_network" {
+    count     = 1
+    name      = format("VM_without_image-%02d", count.index + 1)
+    basedir = format("VM_without_image-%02d", count.index + 1)
+    cpus      = 3
+    memory    = 500
+
+    network_adapter {
+        index = 1
+        network_mode = "null"
+    }
+    network_adapter {
+        index = 2
+        network_mode = "nat"
+        nic_type = "82540EM"
+        cable_connected = true
+    }
+    network_adapter {
+        index = 3
+        network_mode = "hostonly"
+    }
+    network_adapter {
+        index = 4
+        network_mode = "bridged"
+        nic_type = "virtio"
+    }
+
+    status = "poweroff"
 }
 
 # resource "virtualbox_server" "VM_ISO" {
