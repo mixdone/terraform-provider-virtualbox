00acad4e01192ca6cc8d4635af9304a0018e716a
d.barsukova <d.barsukova@g.nsu.ru>
2024-03-10 19:44:30 +0700

    locally running tests

--- examples/resources/main.tf
+++ examples/resources/main.tf
@@ -1,16 +1,16 @@
 
 resource "virtualbox_server" "VM_without_image" {
-    count     = 0
+    count     = 1
     name      = format("VM_without_image-%02d", count.index + 1)
     basedir = format("VM_without_image-%02d", count.index + 1)
     cpus      = 3
     memory    = 1000
-    status = "running"
+    status = "poweroff"
     os_id = "Windows7_64"
 }
 
 resource "virtualbox_server" "bad_VM_example" {
-    count     = 1
+    count     = 0
     name      = format("VM_without_image-%02d", count.index + 1)
     basedir = format("VM_without_image-%02d", count.index + 1)
     cpus      = 30
@@ -20,12 +20,12 @@ resource "virtualbox_server" "bad_VM_example" {
 }
 
 resource "virtualbox_server" "VM_VDI" {
-    count     = 0
+    count     = 1
     name      = format("VM_VDI-%02d", count.index + 1)
     basedir = format("VM_VDI-%02d", count.index + 1)
     cpus      = 2
     memory    = 500
-    url =  "https://github.com/ccll/terraform-provider-virtualbox-images/releases/download/ubuntu-15.04/ubuntu-15.04.tar.xz"
+    //url =  "https://github.com/ccll/terraform-provider-virtualbox-images/releases/download/ubuntu-15.04/ubuntu-15.04.tar.xz"
     status = "poweroff"
     vdi_size = 25000
 }
@@ -37,4 +37,4 @@ resource "virtualbox_server" "VM_VDI" {
 #     cpus      = 2
 #     memory    = 500
 #     //image = "C:/Users/vovap/ubuntu-16.04.6-desktop-i386.iso"
-# }
+# }*/
